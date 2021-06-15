2e1f38d692166d5bf15cbe14ed34090e3d24193d
Anonnya-roy-majumder <79361141+Anonnya-roy-majumder@users.noreply.github.com>
2021-06-15 13:04:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    
+ 'use strict'; 
+    
+     $('.venobox').venobox({
+         arrowsColor:'#0d8d65',
+         bgcolor:'#49957b;',
+         spinner:'cube-grid',
+         spinColor:'#837e13;',
+         titleattr:'video of a natural beauty',
+         titleBackground:'#116538;',
+         titlePosition:'bottom',
+     }); 
+    
+});
